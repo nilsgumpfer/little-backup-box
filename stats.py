db07874d6dfa34b9cbe1f790660f01deff134b81
Nils Gumpfer <nils.gumpfer@kite.thm.de>
2019-09-28 21:51:03 +0200

    x

--- stats.py
+++ stats.py
@@ -10,76 +10,89 @@ from PIL import ImageFont
 
 import subprocess
 
-
-def parseargs():
+def parseArgs():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '-text', type=str, help='Text to be displayed')
+
+    parser.add_argument('-s', '-subrun', type=str, action='append', help='ID of the subrun added to each config')
+    parser.add_argument('-g', '-gpuid', type=int, help='ID of GPU to train the model on')
+    parser.add_argument('-f', '-cfrom', type=int, help='ID of config to start with')
+    parser.add_argument('-t', '-cto', type=int, help='ID of config to end with')
+    parser.add_argument('-c', '-clist', type=int, action='append', help='IDs of configs to run - replaces range of -f and -t')
+    parser.add_argument('-r', '-run', type=int, help='ID of run - replaces -f, -t and -c')
+    parser.add_argument('-test', type=bool, help='If model should only be tested (true) or run (false)')
+    parser.add_argument('-log', type=str, help='Loglevel')
     args = parser.parse_args()
 
-    return args.t
+    return args.s, args.g, args.f, args.t, args.c, args.test, args.r, args.log
+
+# Raspberry Pi pin configuration:
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# 128x32 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
 
 
-def main():
-    # Raspberry Pi pin configuration:
-    RST = None  # on the PiOLED this pin isnt used
-    # Note the following are only used with SPI:
-    DC = 23
-    SPI_PORT = 0
-    SPI_DEVICE = 0
+# Load default font.
+font = ImageFont.load_default()
 
-    # 128x32 display with hardware I2C:
-    disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('Minecraftia.ttf', 8)
 
-    # Initialize library.
-    disp.begin()
-
-    # Clear display.
-    disp.clear()
-    disp.display()
-
-    # Create blank image for drawing.
-    # Make sure to create image with mode '1' for 1-bit color.
-    width = disp.width
-    height = disp.height
-    image = Image.new('1', (width, height))
-
-    # Get drawing object to draw on image.
-    draw = ImageDraw.Draw(image)
-
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-    # Draw some shapes.
-    # First define some constants to allow easy resizing of shapes.
-    padding = -2
-    space = 8
-    top = padding
-    bottom = height - padding
-    pos = 0
-    # Move left to right keeping track of the current x position for drawing shapes.
-    x = 0
-
-    # Load default font.
-    font = ImageFont.load_default()
-
-    # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-    # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-    # font = ImageFont.truetype('Minecraftia.ttf', 8)
-
-    text = parseargs()
-    lines = text.splitlines()
+while True:
 
     # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
-    
-    for line in lines:
-        # Write two lines of text.
-        draw.text((x, top + pos), line, font=font, fill=255)
-        pos += space
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d\' \' -f1"
+    IP = subprocess.check_output(cmd, shell = True )
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    CPU = subprocess.check_output(cmd, shell = True )
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell = True )
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+    Disk = subprocess.check_output(cmd, shell = True )
+
+    # Write two lines of text.
+
+    draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
+    draw.text((x, top+8),     str(CPU), font=font, fill=255)
+    draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
+    draw.text((x, top+25),    str(Disk),  font=font, fill=255)
 
     # Display image.
     disp.image(image)
     disp.display()
-
-
-main()
+    time.sleep(.1)
