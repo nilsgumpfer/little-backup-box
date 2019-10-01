69fbc3b2bfeacf3bc1de11736dc8462d19360e07
nilsgumpfer <nils.gumpfer@kite.thm.de>
2019-10-01 20:52:09 +0200

    test

--- scripts/card-backup.sh
+++ scripts/card-backup.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 # IMPORTANT:
 # Run the install-little-backup-box.sh script first
 # to install the required packages and configure the system.
@@ -23,8 +10,6 @@ STORAGE_DEV="sdb1" # Name of the storage device
 STORAGE_MOUNT_POINT="/media/storage" # Mount point of the storage device
 CARD_DEV="sda1" # Name of the storage card
 CARD_MOUNT_POINT="/media/card" # Mount point of the storage card
-SHUTD="5" # Minutes to wait before shutdown due to inactivity
-
 
 # Print on display
 sudo python3 /home/pi/little-backup-box/scripts/display.py -t "  Insert USB device"
@@ -33,7 +18,7 @@ sudo python3 /home/pi/little-backup-box/scripts/display.py -t "  Insert USB devi
 STORAGE=$(ls /dev/sd* | grep "$STORAGE_DEV" | cut -d"/" -f3)
 while [ -z "${STORAGE}" ]
   do
-  sleep 1
+  sleep 0.1
   STORAGE=$(ls /dev/sd* | grep "$STORAGE_DEV" | cut -d"/" -f3)
 done
 
@@ -47,15 +32,13 @@ sudo python3 /home/pi/little-backup-box/scripts/display.py -t "   Insert SD card
 CARD_READER=($(ls /dev/sd* | grep "$CARD_DEV" | cut -d"/" -f3))
 until [ ! -z "${CARD_READER[0]}" ]
   do
-  sleep 1
+  sleep 0.1
   CARD_READER=($(ls /dev/sd* | grep "$CARD_DEV" | cut -d"/" -f3))
 done
 
 # If card was detected, mount it and obtain its UUID
 if [ ! -z "${CARD_READER[0]}" ]; then
   mount /dev"/${CARD_READER[0]}" "$CARD_MOUNT_POINT"
-
-  echo "SD card mounted. Setting things up.."
 
   CARD_COUNT=$(find $CARD_MOUNT_POINT/ -type f | wc -l)
 
@@ -72,6 +55,7 @@ if [ ! -z "${CARD_READER[0]}" ]; then
   # Set the backup path
   BACKUP_PATH="$STORAGE_MOUNT_POINT"/"$ID"
   STORAGE_COUNT=$(find $BACKUP_PATH/ -type f | wc -l)
+  #TOTRANSFER=$(expr 100 \* $STORAGE_COUNT / $CARD_COUNT)
 
   # Perform backup using rsync
   rsync -avh --info=progress2 --exclude "*.id" "$CARD_MOUNT_POINT"/ "$BACKUP_PATH" &
@@ -84,7 +68,7 @@ if [ ! -z "${CARD_READER[0]}" ]; then
 
     sudo python3 /home/pi/little-backup-box/scripts/display.py -t "Progress:  $PERCENT %" -t "Files:  $STORAGE_COUNT / $CARD_COUNT"
     
-    sleep 1
+    sleep 0.1
   done
 fi
 
